d4b45b28f8bc33c6faa4c1eb1758537f1503af55
leehojun0303 <54626564+leehojun0303@users.noreply.github.com>
2026-09-11 10:06:57 +0900

    Update supabase-config.js

--- dist/supabase-config.js
+++ dist/supabase-config.js
@@ -3,7 +3,7 @@
  * Never put a service-role key in this file.
  */
 window.SUPABASE_CONFIG = Object.assign({
-  url: '',
-  anonKey: '',
-  functionsBase: ''
+  url: 'https://ohsjhyzomfirnlmohzjt.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oc2poeXpvbWZpcm5sbW9oemp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNzY4MjcsImV4cCI6MjEwNDY1MjgyN30.4G84MgzAo5RIeq8Lhb-qhUXi3PpT0EuThHKKcDtFxBI',
+  functionsBase: 'https://ohsjhyzomfirnlmohzjt.supabase.co/functions/v1'
 }, window.SUPABASE_CONFIG || {});
